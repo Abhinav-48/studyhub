e5dfaeac3eeaa2e05b1fa74e16cfd32b8fe0352f
Abhinav-48 <abhinav123451gupta@gmail.com>
2026-07-12 18:14:41 +0530

    Fix missing cloudinary.config() causing API key error

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ const b2 = new S3Client({
   }
 });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 let onlineUsers = {};
 
 const upload = multer({
